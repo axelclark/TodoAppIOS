73dd4b51188064ec369aa2d71c1b339b4ac6cd95
Axel Clark <axelclark2@yahoo.com>
2018-04-13 23:29:16 -0400

    Add basic todo list

--- App.js
+++ App.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react'
 import { View, ScrollView, StyleSheet, Text } from 'react-native'
- 
+import Heading from './components/Heading'
+import Input from './components/Input'
+import Button from './components/Button'
+import TodoList from './components/TodoList'
+
+let todoIndex = 0
+
 class App extends Component {
-  state = {
-    inputValue: '',
-    todos: [],
-    type: 'All'
+  constructor() {
+    super()
+    this.state = {
+      inputValue: '',
+      todos: [],
+      type: 'All'
+    }
+    this.submitTodo = this.submitTodo.bind(this)
+  }
+  inputChange(inputValue) {
+    console.log('Input Value:: ', inputValue)
+    this.setState({ inputValue })
+  }
+  submitTodo() {
+    if (this.state.inputValue.match(/^\s*$/)) {
+      return
+    }
+    let todo = {
+      title: this.state.inputValue,
+      todoIndex: todoIndex,
+      complete: false
+    }
+    todoIndex++
+    this.state.todos.push(todo)
+    this.setState({ todos: this.state.todos, inputValue: '' }, () => {
+      console.log('State: ', this.state)
+    })
   }
   render() {
+    const { inputValue, todos } = this.state
     return (
       <View style={styles.container}>
         <ScrollView
          keyboardShouldPersistTaps="always"
          style={styles.content}>
+         <Heading />
+         <Input
+           inputValue={inputValue}
+           inputChange={(text) => this.inputChange(text)}
+         />
+         <TodoList todos={todos} />
+         <Button submitTodo={this.submitTodo}/>
         </ScrollView>
       </View>
   )
